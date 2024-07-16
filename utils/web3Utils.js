7f3000030654901fbaa0669cd5d6604128186f5a
realdiganta <47485188+dxganta@users.noreply.github.com>
2024-07-16 10:18:10 +0530

    quartz price update

--- utils/web3Utils.js
+++ utils/web3Utils.js
@@ -112,7 +112,7 @@ export async function getPrices() {
   const data = await response.json();
 
   // console.log("data", data.data.QUARTZ[0].quote.USD.)
-  const quartzPrice = 0.1678; // using a hardcoded price since coinmarketcap has removed price tracking of quartz
+  const quartzPrice = 0.1547; // using a hardcoded price since coinmarketcap has removed price tracking of quartz
   const ethPrice = data.data.ETH[0].quote.USD.price.toFixed(2);
 
   return [ ethPrice, quartzPrice];
