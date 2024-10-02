167d01a30bff377e3c51b89890ea2a74135f8d33
realdiganta <47485188+dxganta@users.noreply.github.com>
2024-10-02 13:18:34 +0530

    getLtvForLiquidationPrice

--- ltvs/liquidation.js
+++ ltvs/liquidation.js
@@ -3,6 +3,7 @@ import Web3 from "web3";
 import { vaults, blacklistedAddress } from "../utils/utils.js";
 import scUsdcAbi from "../abis/scUsdcAbi.json" with { type: "json" };
 import ERC20Abi from "../abis/ERC20Abi.json" with { type: "json" };
+import { getPrices } from "../utils/web3Utils.js";
 
 
 dotenv.config();
@@ -19,8 +20,21 @@ const getLiquidationPrice =  async (maxLtv, web3, vaultAddress) => {
     const totalDebt = Number(await vault.methods.totalDebt().call());
 
     const liquidationPrice = (totalCollateral * maxLtv * 10e11) / totalDebt;
-    console.log("liquidation price", liquidationPrice);
+    console.log("ltv:", maxLtv.toFixed(2), "liquidation price $ETH", liquidationPrice.toFixed(0));
     return liquidationPrice;
 }
 
-getLiquidationPrice(0.7, web3, vaults.scusdc);
+const getLtvForLiquidationPrice = async (liquidationPrice, maxLtv) => {
+
+ const [ethPrice, _] = await getPrices();
+ const requiredLtv = (maxLtv * ethPrice) / liquidationPrice;
+
+ console.log("ltv:", requiredLtv.toFixed(2), "liquidation price $ETH", liquidationPrice.toFixed(0));
+ return requiredLtv;
+}
+
+let lp = 3000;
+while (lp <= 4300) {
+    await getLtvForLiquidationPrice(lp, 0.7, web3, vaults.scusdc);
+    lp += 250;
+}
